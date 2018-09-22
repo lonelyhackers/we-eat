2821bd0611bea2271276f1f5ed87cbeaec567613
DLo930 <DLo930>
2018-09-22 16:57:49 -0400

    Update to export.mjs

--- export.mjs
+++ export.mjs
@@ -1,12 +1,12 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://dennislo930:abcd123!@cluster0-iyi5w.mongodb.net/admin";
+var url = "mongodb+srv://dennislo930:Letmein24!@cluster0-iyi5w.mongodb.net/admin";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("we-eat");
-  dbo.collection("profiles").findOne("Anoop Bhat", function(err, result) {
+  dbo.collection("profiles").findOne({"Name":"Anoop Bhat"}, function(err, result) {
     if (err) throw err;
-    console.log(result.name);
+    console.log(result.Prefs);
     db.close();
   });
-}); 
+});
--- import.mjs
+++ import.mjs
@@ -1,10 +1,10 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://dennislo930:abcd123!@cluster0-iyi5w.mongodb.net/admin";
+var url = "mongodb+srv://dennislo930:Letmein24!@cluster0-iyi5w.mongodb.net/admin";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("we-eat");
-  var myobj = require('C:/Users/denni/we-eat/profiles/profile0.json');
+  var myobj = require('C:/Users/denni/we-eat/profiles/profile1.json');
   dbo.collection("profiles").insertOne(myobj, function(err, res) {
     if (err) throw err;
     console.log("1 document inserted");
