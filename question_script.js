c11b575968f42be16703dcec1b9c9f1b0d9b7fa6
EpicDuckPotato <noopy.g.bhat@gmail.com>
2018-09-21 23:27:04 -0400

    Update question_script.js

--- question_script.js
+++ question_script.js
@@ -3,14 +3,31 @@ var next_button = document.getElementById('next');
 var input_name = document.getElementById('name');
 var distance = document.getElementById('distance');
 
+function getSelectValues(select) {
+  var result = [];
+  var options = select && select.options;
+  var opt;
+
+  for (var i=0, iLen=options.length; i<iLen; i++) {
+    opt = options[i];
+
+    if (opt.selected) {
+      result.push(opt.value || opt.text);
+    }
+  }
+  return result;
+}
+
 prefs.onclick = function(){
   next_button.disabled = false;
 }
 
+pref_list = getSelectValues(prefs);
+
 next_button.onclick = function(){
   var prefs_param = '';
-  for (var i = 0; i < prefs.val().length; i++){
-    prefs_param += '&prefs=' + prefs.val()[i];
+  for (var i = 0; i < pref_list.length; i++){
+    prefs_param += '&prefs=' + pref_list.val()[i];
   }
   //window.location.href = 'profileTemplate.html'
   window.location.href = 'profileTemplate.html?name=' + input_name.value + '&distance=' + distance.value + prefs_param; 
