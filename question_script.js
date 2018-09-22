f0f56f07ae119d482c23168896017205a730bdc4
EpicDuckPotato <noopy.g.bhat@gmail.com>
2018-09-21 22:30:32 -0400

    Update question_script.js

--- question_script.js
+++ question_script.js
@@ -1,13 +1,3 @@
 document.getElementById('prefs').onclick = function(){
   document.getElementById('next').disabled = false;
 }
-/*while(true){
-  var prefs = document.getElementById('prefs');
-  if(prefs.val().length === 0){
-    document.getElementById('next').disabled = true;
-  }else{
-    document.getElementById('next').disabled = false;
-  }
-  document.getElementById('test').value = prefs.val().length;
-}
-*/
