26e7c278bbe61ec832ec90b583b4ec15006c018f
EpicDuckPotato <noopy.g.bhat@gmail.com>
2018-09-22 15:18:27 -0400

    Update pfTemp_script.js

--- pfTemp_script.js
+++ pfTemp_script.js
@@ -66,8 +66,8 @@ function get_profile(number) {
 
 function get_nearby_restaurants(formatted_url){
   var xobj = new XMLHttpRequest();
-  xobj.setRequestHeader('Authorization','Bearer Zm7gV6RHPno_RB4Kclkda_mc_Q7nAh7R72Iju71zoY9HGxfaXqUqXALMrT4adBC8kUVr5FdPI9CDrG2zCWUJnjT36o73X8JFBqK-YhprJeANbGSbNr5QZQGzIIymW3Yx');
   xobj.open("GET", url, false);
+  xobj.setRequestHeader('Authorization','Bearer Zm7gV6RHPno_RB4Kclkda_mc_Q7nAh7R72Iju71zoY9HGxfaXqUqXALMrT4adBC8kUVr5FdPI9CDrG2zCWUJnjT36o73X8JFBqK-YhprJeANbGSbNr5QZQGzIIymW3Yx');
   xobj.send();
   xobj.onreadystatechange = function(){
     var textobj = JSON.parse(xobj.responseText);
