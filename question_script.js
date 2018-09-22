b2fe4be89ff31f650f02183fd0cabca2265a0674
EpicDuckPotato <noopy.g.bhat@gmail.com>
2018-09-21 23:28:15 -0400

    Update question_script.js

--- question_script.js
+++ question_script.js
@@ -27,7 +27,7 @@ pref_list = getSelectValues(prefs);
 next_button.onclick = function(){
   var prefs_param = '';
   for (var i = 0; i < pref_list.length; i++){
-    prefs_param += '&prefs=' + pref_list.val()[i];
+    prefs_param += '&prefs=' + pref_list[i];
   }
   //window.location.href = 'profileTemplate.html'
   window.location.href = 'profileTemplate.html?name=' + input_name.value + '&distance=' + distance.value + prefs_param; 
