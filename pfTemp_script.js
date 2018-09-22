ee52c8e9d1d39c02b12d16749761b8efe2bcd2b8
EpicDuckPotato <noopy.g.bhat@gmail.com>
2018-09-22 11:29:49 -0400

    Update pfTemp_script.js

--- pfTemp_script.js
+++ pfTemp_script.js
@@ -6,3 +6,49 @@ var prefs = url.searchParams.get('prefs');
 var latitude = url.searchParams.get('latitude');
 var longitude = url.searchParams.get('longitude');
 
+/*
+for(var i = 0; i < get_num_profiles(); i++){
+  var profile = get_profile(i);
+  
+}*/
+
+//testing ability to get profile info from json
+var profile = get_profile(1);
+console.log(profile.name);
+
+//haversine formula
+function calc_distance(lat1,lon1,lat2,lon2){//in degrees
+  var R = 6371e3; //radius of earth in meters
+  var φ1 = lat1.toRadians();
+  var φ2 = lat2.toRadians();
+  var Δφ = (lat2-lat1).toRadians();
+  var Δλ = (lon2-lon1).toRadians();
+
+  var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+          Math.cos(φ1) * Math.cos(φ2) *
+          Math.sin(Δλ/2) * Math.sin(Δλ/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  var d = R * c;
+  
+  return d;
+}
+
+//somehow access database and get the number of profiles
+function get_num_profiles(){
+  return 1;
+}
+
+//code to load json from stackoverflow. hopefully it works
+function get_profile(number) {   
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'profiles/profile' + String(number) + '.json', true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            return JSON.parse(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
