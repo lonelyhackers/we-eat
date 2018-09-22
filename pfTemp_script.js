4cde134e8e9efffbcbc0bc50c5cb59929fc9df0e
EpicDuckPotato <noopy.g.bhat@gmail.com>
2018-09-22 17:49:04 -0400

    Update pfTemp_script.js

--- pfTemp_script.js
+++ pfTemp_script.js
@@ -82,7 +82,7 @@ function get_nearby_restaurants(formatted_url){
       request.withCredentials = true;
       request.setRequestHeader('Authorization','Bearer Zm7gV6RHPno_RB4Kclkda_mc_Q7nAh7R72Iju71zoY9HGxfaXqUqXALMrT4adBC8kUVr5FdPI9CDrG2zCWUJnjT36o73X8JFBqK-YhprJeANbGSbNr5QZQGzIIymW3Yx'); 
     },
-    //headers: {'Authorization' : 'Bearer Zm7gV6RHPno_RB4Kclkda_mc_Q7nAh7R72Iju71zoY9HGxfaXqUqXALMrT4adBC8kUVr5FdPI9CDrG2zCWUJnjT36o73X8JFBqK-YhprJeANbGSbNr5QZQGzIIymW3Yx'},
+    headers: {'Authorization' : 'Bearer Zm7gV6RHPno_RB4Kclkda_mc_Q7nAh7R72Iju71zoY9HGxfaXqUqXALMrT4adBC8kUVr5FdPI9CDrG2zCWUJnjT36o73X8JFBqK-YhprJeANbGSbNr5QZQGzIIymW3Yx'},
     url: formatted_url,
     dataType : 'jsonp',
     success: function(msg) {
