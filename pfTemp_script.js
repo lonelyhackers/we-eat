28f7ee0d5e83762290573537fce8b106b9dbc687
EpicDuckPotato <noopy.g.bhat@gmail.com>
2018-09-22 12:58:17 -0400

    Update pfTemp_script.js

--- pfTemp_script.js
+++ pfTemp_script.js
@@ -2,7 +2,7 @@ var url_string = window.location.href;
 var url = new URL(url_string);
 var name = url.searchParams.get('name');
 var distance = url.searchParams.get('distance');
-var prefs = url.searchParams.get('prefs');
+var prefs = url.searchParams.get('prefs[]');
 var latitude = url.searchParams.get('latitude');
 var longitude = url.searchParams.get('longitude');
 
