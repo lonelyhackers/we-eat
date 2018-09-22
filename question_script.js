7abc2a01fc89d8b229bddbeb11e59edfd06cfeac
EpicDuckPotato <noopy.g.bhat@gmail.com>
2018-09-21 22:07:17 -0400

    Update question_script.js

--- question_script.js
+++ question_script.js
@@ -1,7 +1,7 @@
 while(true){
   var prefs = document.getElementById('prefs');
   if(prefs.val().length == 0){
-    document.getElementById('next').disabled = false;
+    document.getElementById('next').disabled = true;
   }else{
     document.getElementById('next').disabled = true;
   }
